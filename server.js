be4660186cd3f63a48de261702c8c549ab55dec8
Brad Schoonover <badonmap@WIN2.CS.KSU.EDU>
2017-04-20 14:21:29 -0500

    started creating secure servers and test enviroments

--- server.js
+++ server.js
@@ -9,12 +9,23 @@ const PORT = 3000;
 var fs = require('fs');
 var http = require('http');
 var express = require('express');
+var config = reuire('./config.json');
 
 // The Express app
 var app = express();
 
 // Serve files from public folder
 app.use(express.static('public'));
+
+var options = {
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+}
+
+var server = https.createServer(options, app);
+
+var authCAS = require('auth-cas');
+var auth = new AuthCAS(config.host, config.casHost)
 
 // Start the server
 app.listen(PORT, function(){
